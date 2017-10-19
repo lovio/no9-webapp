886834855958f33df83914a5da271c99cc007760
Kris Liu <liuqi0611@126.com>
2017-10-19 14:58:25 +0800

    replace link with history

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import history from 'helpers/history';
 import map from 'lodash/map';
 
 import { Footer } from 'ui';
@@ -37,34 +37,32 @@ const Tab = styled.div`
   width: 100%;
   text-align: center;
 
-  a {
-    display: block;
-    text-decoration: none;
-    // 还有border-top的0.01rem
-    margin: 0.07rem 0 0.04rem;
-    border-right: 1px solid #dbdcdd;
+  display: block;
+  text-decoration: none;
+  // 还有border-top的0.01rem
+  margin: 0.07rem 0 0.04rem;
+  border-right: 1px solid #dbdcdd;
 
-    img {
-      height: 0.14rem;
-    }
-
-    p {
-      /* 资产: */
-      line-height: 0.2rem;
-      font-size: 0.14rem;
-      margin: 0;
-      letter-spacing: 0;
-      /* 首页: */
-      color: ${(props) => {
-        if (props.isActive) {
-          return '#E01053';
-        }
-        return '#4A4A4A';
-      }};
-    }
+  img {
+    height: 0.14rem;
   }
 
-  &:last-of-type > a {
+  p {
+    /* 资产: */
+    line-height: 0.2rem;
+    font-size: 0.14rem;
+    margin: 0;
+    letter-spacing: 0;
+    /* 首页: */
+    color: ${(props) => {
+      if (props.isActive) {
+        return '#E01053';
+      }
+      return '#4A4A4A';
+    }};
+  }
+
+  &:last-of-type {
     border-right: none;
   }
 `;
@@ -73,11 +71,17 @@ export default function FooterView({ location: { pathname } }) {
   return (
     <Footer>
       {map(LINKS, link => (
-        <Tab key={link.url} isActive={pathname === link.url}>
-          <Link to={link.url}>
-            <img src={link.img} alt="property" />
-            <p>{link.title}</p>
-          </Link>
+        <Tab
+          key={link.url}
+          isActive={pathname === link.url}
+          onClick={() => {
+            if (link.url !== history.location.pathname) {
+              history.push(link.url);
+            }
+          }}
+        >
+          <img src={link.img} alt="property" />
+          <p>{link.title}</p>
         </Tab>
       ))}
     </Footer>
