c9600a7bd2ba13224a2b7693e161e420912df704
Kris Liu <liuqi0611@126.com>
2017-10-20 18:08:30 +0800

    better AuthenticatedHOC

--- src/containers/auth/AuthenticatedHOC.jsx
+++ src/containers/auth/AuthenticatedHOC.jsx
@@ -1,40 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { checkAuth } from 'actions/auth';
+import * as authActions from 'actions/auth';
 
 export default function requireAuthentication(Component) {
-  class AuthenticatedComponent extends React.Component {
-    static defaultProps = {
-      token: '',
-    };
+  const AuthenticatedComponent = ({ checkAuth, ...others }) => {
+    checkAuth();
+    return <Component {...others} />;
+  };
 
-    static propTypes = {
-      token: PropTypes.string,
-      checkAuth: PropTypes.func.isRequired,
-    };
-    constructor(props) {
-      super(props);
-      this.props.checkAuth();
-    }
+  AuthenticatedComponent.propTypes = {
+    checkAuth: PropTypes.func.isRequired,
+  };
 
-    componentWillReceiveProps() {
-      this.props.checkAuth();
-    }
-
-    render() {
-      const { token, ...rest } = this.props;
-      if (!token) {
-        return null;
-      }
-      return <Component {...rest} />;
-    }
-  }
-
-  function mapStateToProps(state) {
-    return {
-      token: state.getIn(['user', 'token']),
-    };
-  }
-  return connect(mapStateToProps, { checkAuth })(AuthenticatedComponent);
+  return connect(null, { checkAuth: authActions.checkAuth })(AuthenticatedComponent);
 }
