2e6740858fecac2e727009ce1b56ab9deb600145
Kris Liu <liuqi0611@126.com>
2017-10-23 22:20:04 +0800

    add persistState for production

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,17 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware, { END } from 'redux-saga';
 import rootReducer from '../reducers';
-// import persistState from './storage';
+import persistState from './storage';
 
 export default function configureStore(initialState) {
   const sagaMiddleware = createSagaMiddleware();
   const store = createStore(
     rootReducer,
     initialState,
-    compose(
-      applyMiddleware(sagaMiddleware),
-      // persistState,
-    ),
+    compose(applyMiddleware(sagaMiddleware), persistState),
   );
   store.runSaga = sagaMiddleware.run;
 
