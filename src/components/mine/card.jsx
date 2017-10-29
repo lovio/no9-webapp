99e7f1db624b8619faa4a67413d768af01367236
Kris Liu <liuqi0611@126.com>
2017-10-29 10:58:03 +0800

    update home page

--- src/components/mine/card.jsx
+++ src/components/mine/card.jsx
@@ -1,95 +1,120 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import map from 'lodash/map';
 import styled from 'styled-components';
 import Slider from 'ui/slider';
 import { GRADE_MAPPING } from '../../constants/constants.json';
 
-import imgCardBg from './card-bg.jpg';
-import imgCrown from './crown.png';
-import imgMemberIntro from './member-intro.jpg';
+import imgCrown1 from './crown1.png';
+import imgCrown2 from './crown2.png';
+import imgCrown3 from './crown3.png';
+import imgCrown4 from './crown4.png';
+import imgCrown5 from './crown5.png';
 
-const Container = styled.div`padding: 0.35rem 0.25rem;`;
+const crownMapping = {
+  0: imgCrown1,
+  1: imgCrown1,
+  2: imgCrown2,
+  3: imgCrown3,
+  4: imgCrown4,
+  5: imgCrown5,
+};
+
+const gradeStyles = {
+  0: {
+    color: '#ACBDE0',
+    bg: 'background-image: linear-gradient(-195deg, #899CBB 0%, #596A85 100%);',
+  },
+  1: {
+    color: '#ACBDE0',
+    bg: 'background-image: linear-gradient(-195deg, #899CBB 0%, #596A85 100%);',
+  },
+  2: {
+    color: '#C7D7D5',
+    bg: 'background-image: linear-gradient(-193deg, #93BBB8 0%, #4C746F 100%);',
+  },
+  3: {
+    color: '#E0E0E1',
+    bg: 'background-image: linear-gradient(-199deg, #CED0D6 0%, #949AA2 100%)',
+  },
+  4: {
+    color: '#F8E2C1',
+    bg: 'background-image: linear-gradient(-191deg, #EBD1A9 0%, #BE9C68 100%)',
+  },
+  5: {
+    color: '#DAA848',
+    bg: 'background-image: linear-gradient(-190deg, #5D5F63 0%, #252527 95%)',
+  },
+};
+
+const Container = styled.div`
+  padding: 0.35rem 0.25rem;
+  background-color: #1a1515;
+`;
 
 const Info = styled.div`
   height: 1.7rem;
   border-radius: 0.1rem;
-  background-image: url("${imgCardBg}");
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
+  ${props => props.bg};
 `;
 
-const MemberIntro = styled(Info)`
-  background-image: url("${imgMemberIntro}");
+const TitleContainer = styled.div`
+  position: relative;
+  div {
+    position: absolute;
+    top: -0.25rem;
+    left: 0;
+    right: 0;
+    img {
+      margin: 0 auto;
+      width: 0.5rem;
+    }
+  }
 `;
 
-const TitleContainer = styled.div`position: relative;`;
+const BaseP = styled.p`color: ${props => props.color};`;
 
-const Title = styled.div`
-  position: absolute;
-  top: -0.29rem;
-  display: flex;
-  flex-direction: column;
-  justify-content:center;
-  margin-left: 1.37rem;
-  width: 0.64rem;
-  height: 0.58rem;
-  background-image: url("${imgCrown}");
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  text-align: center;
-  padding-right:0.1rem;
-
-`;
-
-const TitleName = styled.p`
-  line-height: 0.18rem;
-  font-size: 0.12rem;
-  color: #e01053;
-`;
-
-const Item = styled.p`
+const Item = styled(BaseP)`
   margin-top: 0.05rem;
   text-align: center;
-  font-size: 0.14rem;
   line-height: 0.2rem;
-  color: rgba(255, 255, 255, 0.8);
+  font-size: 0.18rem;
 `;
 
 const Name = styled(Item)`
   margin-top: 0;
   padding-top: 0.33rem;
+  font-size: 0.2rem;
 `;
 
 const No = styled(Item)`
   margin-top: 0.6rem;
   padding-left: 0.15rem;
   text-align: left;
+  font-size: 0.14rem;
 `;
 
-const Card = ({ user }) => (
-  <Slider disableAutoPlay>
-    <Container>
-      <Info>
-        <TitleContainer>
-          <Title>
-            {map(GRADE_MAPPING[user.get('grade')], title => (
-              <TitleName key={title}>{title}</TitleName>
-            ))}
-          </Title>
-        </TitleContainer>
-        <Name>{user.get('name')}</Name>
-        <Item>打败了全国 98%的用户</Item>
-        <No>账号编号：{user.get('phone')}</No>
-      </Info>
-    </Container>
-    <Container>
-      <MemberIntro />
-    </Container>
-  </Slider>
-);
+const Card = ({ user }) => {
+  const grade = user.get('grade');
+  const style = gradeStyles[grade];
+
+  return (
+    <Slider disableAutoPlay>
+      <Container>
+        <Info bg={style.bg}>
+          <TitleContainer>
+            <div>
+              <img src={crownMapping[grade]} alt="" />
+            </div>
+          </TitleContainer>
+          <Name color={style.color}>{GRADE_MAPPING[grade]}</Name>
+          <Item color={style.color}>{user.get('name')}</Item>
+          <No color={style.color}>账号编号：{user.get('phone')}</No>
+        </Info>
+      </Container>
+      {/* <Container></Container> */}
+    </Slider>
+  );
+};
 
 Card.propTypes = {
   user: PropTypes.object.isRequired,
