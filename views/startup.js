c6dc00ff17137ae236233e51808069e75722eb8e
Kris Liu <liuqi0611@126.com>
2017-11-02 17:39:49 +0800

    fix url repalce

--- views/startup.js
+++ views/startup.js
@@ -1,15 +1,15 @@
 // 这个脚本是干哈呢?
-(function () {
+(function() {
   const searchQuery = location.search.split('?')[1];
   if (!searchQuery) {
     return;
   }
   const hashQuery = location.hash.split('?')[1];
-  let newUrl = `//${location.host}${location.hash ? location.hash : '/#/'}`;
+  let newUrl = `//${location.host}/${location.hash ? location.hash : '#/'}`;
   if (!hashQuery) {
     newUrl = `${newUrl}?${searchQuery}`;
   } else if (searchQuery) {
     newUrl = `${newUrl}&${searchQuery}`;
   }
   location.replace(newUrl);
-}());
+})();
