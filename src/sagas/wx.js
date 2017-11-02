5d21f876d37bd75ac4d86e8e93998c05ee493deb
Kris Liu <liuqi0611@126.com>
2017-11-02 17:48:45 +0800

    use call instead of fork

--- src/sagas/wx.js
+++ src/sagas/wx.js
@@ -25,7 +25,7 @@ export function* watchCode() {
     const { payload } = yield take(actions.transferCode);
     if (inWechat) {
       yield call(delay, 500);
-      yield fork(wxHelper.transferCode, payload || {});
+      yield call(wxHelper.transferCode, payload || {});
     }
   }
 }
