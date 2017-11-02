c28f46cb3f5984e4de226b00d4847abd5b50ca1e
Kris Liu <liuqi0611@126.com>
2017-11-02 10:29:24 +0800

    remove storage in prod

--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware, { END } from 'redux-saga';
-import persistState from './storage';
+// import persistState from './storage';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
   const sagaMiddleware = createSagaMiddleware();
-  const store = createStore(rootReducer, initialState, compose(applyMiddleware(sagaMiddleware), persistState));
+  // const store = createStore(rootReducer, initialState, compose(applyMiddleware(sagaMiddleware), persistState));
+  const store = createStore(rootReducer, initialState, compose(applyMiddleware(sagaMiddleware)));
   store.runSaga = sagaMiddleware.run;
 
   store.close = () => store.dispatch(END);
